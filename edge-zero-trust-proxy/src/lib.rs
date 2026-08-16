8f40faf624d1a5c70224b6c05f7899a2dd55f3bb
Mary-Jane Davies <mjpunkin@outlook.com>
2026-08-16 16:30:35 -0700

    Added necessary API endpoints while still preserving original functionality

--- edge-zero-trust-proxy/src/lib.rs
+++ edge-zero-trust-proxy/src/lib.rs
@@ -5,11 +5,25 @@ fn start_init() {
     console_error_panic_hook::set_once();
 }
 
+async fn reroute_req(_req: Request, _state: RouteContext<()>) -> Result<Response> {
+    // NOOP
+    Response::ok("Called reroute_req")  
+}
+
 #[event(fetch)]
 async fn fetch(
-    _req: Request,
-    _env: Env,
+    req: Request,
+    env: Env,
     _ctx: Context,
 ) -> Result<Response> {
-    Response::ok("Hello World!")
+    Router::new()
+        .get("/", |_, _| {
+            Response::ok("Hello World!")
+        })
+        .get("/health", |_, _| {
+            Response::ok("Edge zero trust proxy server is running")
+        })
+        .on_async("/*path", reroute_req)
+        .run(req, env)
+        .await
 }

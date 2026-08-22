abafb36fc37ce09556802668ba1c21b2d1d348c9
Mary-Jane Davies <mjpunkin@outlook.com>
2026-08-22 15:11:23 -0700

    Refactored so that proxy logic is in its own module

--- edge-zero-trust-proxy/src/lib.rs
+++ edge-zero-trust-proxy/src/lib.rs
@@ -1,55 +1,11 @@
+mod proxy;
+
 use worker::*;
+use proxy::*;
 
 #[event(start)]
 fn start_init() {
     console_error_panic_hook::set_once();
-}
-
-async fn req_debug(req: Request) -> Result<String> {
-    let mut out = String::new();
-
-    out.push_str("========= HEADERS =========\n");
-    for (key, value) in req.headers().entries() {
-        out.push_str(&format!("{}: {}\n", key, value));
-    }
-
-    out.push_str("========= URL =========\n");
-    out.push_str(&format!("URL: {}\n", req.url()?));
-
-    Ok(out)
-
-}
-
-async fn reroute_req(req: Request, _state: RouteContext<()>) -> Result<Response> {
-    let upstream = "https://example.com"; // Temporary backend domain
-    let upstream_url = format!("{}{}", upstream, req.path());
-
-    let headers = req.headers().clone();
-    headers.set("X-Proxy", "EdgeZeroTrust")?; // Temporary header rename
-    headers.delete("Host")?;
-
-    let method = req.method();
-    let body = match method {
-        Method::Get | Method::Head => None,
-        _ => {
-            let mut cloned = req.clone()?;
-            let bytes = cloned.bytes().await?;
-            Some(bytes.into())
-        },
-    }; 
-
-    let init = RequestInit {
-        method,
-        headers,
-        body,
-        cf: CfProperties::default(),
-        ..Default::default()
-    };
-
-    let outbound_req = Request::new_with_init(upstream_url.as_str(), &init)?;
-    
-    // Debug print of computed request header and url
-    Response::ok(req_debug(outbound_req).await?)
 }
 
 #[event(fetch)]
